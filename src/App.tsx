18a8f11e31f2efec5f6c8b5c93ee84368e3f86af
Sameer Ahmed <sameer937655@gmail.com>
2024-07-10 22:39:22 +0530

    enahce the ui of table

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./App.css";
+import { InputBox } from "./components/InputBox";
+import { PlayerBox } from "./components/PlayerBox";
+import { Submit } from "./components/Submit";
+import axios from "axios";
+import { TableRender } from "./components/TableRender";
+
+function App() {
+  const [firstCaptainName, setFirstCaptainName] = useState("");
+  const [secondCaptainName, setSecondCaptainName] = useState("");
+  const [playerName, setPlayerName] = useState("");
+  const [randomPlayerData, setRandomPlayerData] = useState(null);
+
+  async function handleSubmit() {
+    // split take string convert them into an array
+    const playerNamesArray = playerName.split(",").map((name) => name.trim());
+    const data = {
+      firstCaptain: firstCaptainName,
+      secondCaptain: secondCaptainName,
+      playerName: playerNamesArray,
+    };
+
+    try {
+      const randomPlayer = await axios.post(
+        "https://my-app.sameer937655.workers.dev/api/v1/captain",
+        data
+      );
+      setRandomPlayerData(randomPlayer.data);
+    } catch (error) {
+      console.error("Error fetching random player data:", error);
+    }
+  }
+  return (
+    <div className="">
+      <h1 className="flex justify-center text-xl pb-4 font-bold">
+        Randomize Your Team
+      </h1>
+      <InputBox
+        onChange={(e) => {
+          setFirstCaptainName(e.target.value);
+        }}
+        Name="First Captain Name"
+        placeholder="Enter Captain Name"
+      />
+      <InputBox
+        onChange={(e) => {
+          setSecondCaptainName(e.target.value);
+        }}
+        Name="Second Captain Name"
+        placeholder="Enter Captain Name"
+      />
+      <PlayerBox
+        onChange={(e) => {
+          setPlayerName(e.target.value);
+        }}
+        Name="Player Name"
+        placeholder="eg :- sohan,mohan"
+      ></PlayerBox>
+      <Submit onClick={handleSubmit}></Submit>
+      {randomPlayerData && <TableRender randomPlayerData={randomPlayerData} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TableRender.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface PlayerData {
+  [key: string]: string[];
+}
+
+interface TableRenderProps {
+  randomPlayerData: PlayerData;
+}
+
+export const TableRender: React.FC<TableRenderProps> = ({
+  randomPlayerData,
+}) => {
+  return (
+    <div className="mt-2">
+      <div className="flex justify-center">
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+              <tr>
+                {Object.keys(randomPlayerData).map((captain, index) => (
+                  <th key={index} scope="col" className="px-6 py-3">
+                    {captain}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b ">
+                {Object.values(randomPlayerData).map((players, index) => (
+                  <td key={index} className="px-6 py-4">
+                    <ul>
+                      {players.map((player, idx) => (
+                        <li key={idx}>{player}</li>
+                      ))}
+                    </ul>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
