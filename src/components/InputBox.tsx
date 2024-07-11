997b25f1f11a6dd405bb7948e0cd69274e2e7d9b
Sameer Ahmed <sameer937655@gmail.com>
2024-07-11 12:01:27 +0530

    clear tailwind

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -16,7 +16,7 @@ export const InputBox = ({ Name, placeholder, onChange }: Inputypes) => {
         <input
           type="text"
           id="first_name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2"
           placeholder={placeholder}
           onChange={onChange}
           required
